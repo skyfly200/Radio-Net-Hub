f88f64eb6e2e216f3061035f33e5a6c7e61e532e
Skyler Fly-W <skyfly200@gmail.com>
2018-05-02 16:14:57 -0400

    installed npm package(s)

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function() {
+  console.log('listening on 8080')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
