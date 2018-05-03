bbc0b2616402a83ebea19fac0294b558d5706c51
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-05-03 16:13:22 -0600

    tested out db conn, installed body parser. added stations collection.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const port = 8080;
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 
-app.listen(8080, function() {
-  console.log('listening on 8080')
+var url = "mongodb://localhost:27017/radio-net-hub";
+var db_title = 'radio-net-hub';
+
+app.listen(port, function() {
+  console.log('listening on', port)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  MongoClient.connect(url, function(err, conn) {
+    if (err) throw err;
+    var db = conn.db(db_title)
+    // get station from DB
+    db.collection('stations').find().toArray(function(err, results) {
+      // return list of stations in db
+      res.send(results);
+    })
+    conn.close();
+  });
 })
 
+// db.stations.insertOne({ name: "", call_sign: "", website: "", freq: 90.5, location: { locality: "", region: "", country: ""} });
+// db.stations.insertOne({ name: "Way High Radio", call_sign: "KWHR", website: "wayhighradio.com", freq: 90.5, location: { locality: "Ward", region: "CO", country: "USA"} });
