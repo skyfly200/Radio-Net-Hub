aefa776800a18e5da51bad978a0232df4b428eb2
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-05-16 01:38:16 -0600

    station routes updated, all CRUD functions started. DEBUG THIS MORE!

--- app/routes/station_routes.js
+++ app/routes/station_routes.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
     app.get('/stations', (req, res) => {
         // get station from DB
@@ -5,6 +7,17 @@ module.exports = function(app, db) {
             // return list of stations in db
             res.send(results);
         })
+    });
+    app.get('/station/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('stations').findOne(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send(item);
+          } 
+        });
     });
     app.post('/stations', (req, res) => {
         const station = { name: req.body.name, call_sign: req.body.call_sign };
@@ -14,6 +27,28 @@ module.exports = function(app, db) {
             } else {
                 res.send(result.ops[0]);
             }
+        });
+    });
+    app.delete('/station/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('stations').deleteOne(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send('Station ' + id + ' deleted! ' + item);
+          } 
+        });
+     });
+    app.put('/station/:id', (req, res) => {
+        const details = { '_id': new ObjectID(req.params.id) };
+        const station = { name: req.body.name, call_sign: req.body.call_sign, website: req.body.website };
+        db.collection('stations').update(details, station, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(station);
+            } 
         });
     });
 };
